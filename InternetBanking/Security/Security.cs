c4e78e3e03fdbc8d43cad92be5a2d67a9c96a35c
Haicx98 <35750406+Haicx98@users.noreply.github.com>
2018-04-11 09:43:34 +0700

    Security

---
+++ InternetBanking/Security/Security.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetBanking.Security
+{
+    class Security
+    {
+        private static Random random = new Random();
+        private static string RandomString(int length)
+        {
+            const string pool = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[random.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+
+        private static string RandomString1(int length)
+        {
+            const string pool = "0123456789";
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[random.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+
+        public string MD5Encrypt(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
+            byte[] result = md5.Hash;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                stringBuilder.Append(result[i].ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public string SaltRandom()
+        {
+            return RandomString(6);
+        }
+
+        public int BankNumberRandom()
+        {
+            int BankNumber = Int32.Parse(RandomString1(10));
+            return BankNumber;
+        }
+    }
