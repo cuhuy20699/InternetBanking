285bdaadc40975513bbf541ce5ccefd195c87402
Haicx98 <35750406+Haicx98@users.noreply.github.com>
2018-04-11 10:01:41 +0700

    SignIn controller

---
+++ InternetBanking/Controller/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetBanking.Controller
+{
+    class AccountController
+    {
+        public void Signin()
+        {
+            string username;
+            string password;
+            Console.WriteLine("Please Enter your username: ");
+            username = Console.ReadLine();
+            validate.ValidateUserName(username);
+            Console.WriteLine("Please Enter your password: ");
+            password = Console.ReadLine();
+            validate.ValidatePassword(password);
+            AccountModel model = new AccountModel();
+            model.SignIn(username, password);
+        }
+    }
+}
